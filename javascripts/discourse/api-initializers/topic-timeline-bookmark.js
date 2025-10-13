eb4517e6f8d1eb46eb3f9426a26053eeec46bd90
LESP-book <gitbook1917@tutamail.com>
2025-10-13 13:18:10 +0900

    new version 2.0

--- javascripts/discourse/api-initializers/topic-timeline-bookmark.js
+++ javascripts/discourse/api-initializers/topic-timeline-bookmark.js
@@ -1,104 +1,12 @@
-import { getOwner } from "discourse-common/lib/get-owner";
+// javascripts/discourse/initializers/topic-timeline-bookmark.js
 import { apiInitializer } from "discourse/lib/api";
 
-export default apiInitializer("0.11.1", (api) => {
+export default apiInitializer("1.29.0", (api) => {
   const user = api.getCurrentUser();
-  //If User is not logged-in then return, Else proceed
   if (!user) {
     return;
   }
 
-  api.createWidget("topic-timeline-bookmark", {
-    tagName: "div.discourse-bookmark-button-wrapper",
-
-    buildKey: () => `topic-timeline-bookmark`,
-    
-    //Will trigger when topic-timeline bookmark button is clicked. And internally it will call toggleBookmark of the Topic controller to have default bookmark functionality.
-    toggleBookmark() {
-      const topicController = getOwner(this).lookup("controller:topic");
-      topicController.send("toggleBookmark");
-    },
-
-    html(attrs) {
-      //Content (i.e. bookmark button) to be rendered when widget is mounted.
-      let contents = [];
-      //Default Icon, Label and Tooltip values for bookmark button
-      let tooltip = "bookmarked.help.bookmark";
-      let label = "bookmarked.title";
-      let buttonClass = "btn btn-default bookmark";
-      let icon = "bookmark";
-      let bookmarkedPosts = 0;
-      if (attrs.topic) {
-        //Get the bookmarked count for the topic.
-        bookmarkedPosts = attrs.topic.bookmarkCount;
-        //Post(s) already bookmarked then set button Icon, Label and Tooltip accordingly
-        if (bookmarkedPosts > 0) {
-          //Icon
-          if (attrs.topic.bookmarks.some((bookmark) => bookmark.reminder_at)) {
-            icon = "discourse-bookmark-clock";
-          } else {
-            icon = "bookmark";
-          }
-
-          //Label
-          if (bookmarkedPosts === 0) {
-            label = "bookmarked.title";
-          } else if (bookmarkedPosts === 1) {
-            label = "bookmarked.edit_bookmark";
-          } else {
-            label = "bookmarked.clear_bookmarks";
-          }
-
-          //Tooltip
-          if (bookmarkedPosts === 0) {
-            return I18n.t("bookmarked.help.bookmark");
-          } 
-          else if (bookmarkedPosts === 1) {
-            if (
-              attrs.topic.bookmarks.filter((bookmark) => bookmark.for_topic).length
-            ) {
-              tooltip = "bookmarked.help.edit_bookmark_for_topic";
-            } else {
-              tooltip = "bookmarked.help.edit_bookmark";
-            }
-          } else if (attrs.topic.bookmarks.some((bookmark) => bookmark.reminder_at)) {
-            tooltip = "bookmarked.help.unbookmark_with_reminder";
-          } else {
-            tooltip = "bookmarked.help.unbookmark";
-          }
-
-
-          //Append CSS bookmarked class if bookmark is set
-          if (bookmarkedPosts > 0) {
-            buttonClass += " bookmarked";
-          }
-        }
-      }
-
-      //Adding bookmark button to content
-      contents.push(
-        this.attach("button", {
-          action: "toggleBookmark",
-          title: tooltip,
-          label,
-          icon,
-          className: buttonClass,
-        })
-      );
-
-      return contents;
-    },
-
-    bookmarksChanged() {
-      this.scheduleRerender();
-    },
-  });
-
-  //Refresh the topic-timeline-bookmark widget whenever the bookmarks:changed event is triggered.
-  api.dispatchWidgetAppEvent(
-    "topic-timeline-bookmark-container",
-    "topic-timeline-bookmark",
-    "bookmarks:changed"
-  );
-
+  // 在 topic timeline 区域插入我们新的 Glimmer 组件
+  api.renderInOutlet("topic-timeline", "topic-timeline-bookmark");
 });
--- javascripts/discourse/components/topic-timeline-bookmark-container.js
+++ javascripts/discourse/components/topic-timeline-bookmark-container.js
@@ -1,5 +1,75 @@
-import MountWidget from "discourse/components/mount-widget";
+// javascripts/discourse/components/topic-timeline-bookmark.js
+import Component from "@glimmer/component";
+import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
+import { tracked } from "@glimmer/tracking";
+import I18n from "I18n";
+import { subscribe } from "discourse/lib/pub-sub";
 
-export default MountWidget.extend({
-  widget: "topic-timeline-bookmark"
-});
+export default class TopicTimelineBookmark extends Component {
+  @service currentUser;
+  @service store;
+
+  @tracked topic = null;
+
+  constructor() {
+    super(...arguments);
+    this.topic = this.args.outletArgs.topic;
+
+    // 监听书签变更事件
+    subscribe("bookmarks:changed", this, this._onBookmarksChanged);
+  }
+
+  _onBookmarksChanged() {
+    this.topic = this.args.outletArgs.topic;
+  }
+
+  get bookmarkedPosts() {
+    return this.topic?.bookmarkCount || 0;
+  }
+
+  get buttonClass() {
+    let cls = "btn btn-default bookmark";
+    if (this.bookmarkedPosts > 0) cls += " bookmarked";
+    return cls;
+  }
+
+  get icon() {
+    if (!this.topic) return "bookmark";
+    if (this.topic.bookmarks?.some((b) => b.reminder_at)) {
+      return "discourse-bookmark-clock";
+    }
+    return "bookmark";
+  }
+
+  get label() {
+    const count = this.bookmarkedPosts;
+    if (count === 0) return I18n.t("bookmarked.title");
+    if (count === 1) return I18n.t("bookmarked.edit_bookmark");
+    return I18n.t("bookmarked.clear_bookmarks");
+  }
+
+  get tooltip() {
+    const { topic } = this;
+    const count = this.bookmarkedPosts;
+    if (!topic) return I18n.t("bookmarked.help.bookmark");
+
+    if (count === 0) return I18n.t("bookmarked.help.bookmark");
+    if (count === 1) {
+      if (topic.bookmarks?.some((b) => b.for_topic)) {
+        return I18n.t("bookmarked.help.edit_bookmark_for_topic");
+      }
+      return I18n.t("bookmarked.help.edit_bookmark");
+    }
+    if (topic.bookmarks?.some((b) => b.reminder_at)) {
+      return I18n.t("bookmarked.help.unbookmark_with_reminder");
+    }
+    return I18n.t("bookmarked.help.unbookmark");
+  }
+
+  @action
+  toggleBookmark() {
+    const topicController = this.store.lookup("controller:topic");
+    topicController.send("toggleBookmark");
+  }
+}
